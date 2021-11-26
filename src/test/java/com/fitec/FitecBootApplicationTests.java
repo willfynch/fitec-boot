cceb4a5ce9d70122f6e8200a794b556bb6877731
willfynch <william.finzy@protonmail.com>
2021-11-26 16:04:43 +0100

    add test

--- src/test/java/com/fitec/FitecBootApplicationTests.java
+++ src/test/java/com/fitec/FitecBootApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fitec;
 
+import com.fitec.service.Calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FitecBootApplicationTests {
 
+	private Calculator calculator = new Calculator();
+
 	@Test
-	void contextLoads() {
+	public void testSum() {
+		assertEquals(45, calculator.sum(20, 25));
 	}
 
 }
